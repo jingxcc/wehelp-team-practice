1a20307911d2cef6485bbcef7cd371c4ee2f2ab1
yashan <leeys2423@gmail.com>
2023-10-18 02:16:42 +0800

    add e-paper api

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
 
+from routes.weahter import weather_bp
+from e_paper import e_paper
+from db_connection import DB_Connector
+
 app = Flask(__name__)
+app.register_blueprint(weather_bp)
+app.register_blueprint(e_paper)
+
+app.json.ensure_ascii = False
+app.json.sort_keys = False
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['cnx_pool'] = DB_Connector()
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
 
 if __name__ == "__main__":
     app.debug = True
