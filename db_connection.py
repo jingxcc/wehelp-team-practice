3653077beb576e209db45262ec6c7afe191433b5
yashan <leeys2423@gmail.com>
2023-10-19 20:40:49 +0800

    fix db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -4,33 +4,33 @@ import mysql.connector
 from dotenv import load_dotenv
 
 class DB_Connector:
-	_instance = None
+    _instance = None
 
-	def __new__(cls):
-		if cls._instance is None:
-			cls._instance = super(DB_Connector, cls).__new__(cls)
-			cls._instance.init_connection_pool()
-			return cls._instance
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DB_Connector, cls).__new__(cls)
+            cls._instance.init_connection_pool()
+        return cls._instance
 
-	def init_connection_pool(self):
-		load_dotenv()
-		db_config = {
-			'host': 'localhost',
-			'database': 'weather_assistant',
-			'user': os.getenv('db_user'),
-			'password': os.getenv('db_password')
-		}
-		self.cnx_pool = mysql.connector.pooling.MySQLConnectionPool(
-			pool_name = 'conn_pool',
-			pool_size = 15,
-			**db_config
-		)
+    def init_connection_pool(self):
+        load_dotenv()
+        db_config = {
+            'host': 'localhost',
+            'database': 'weather_assistant',
+            'user': os.getenv('db_user'),
+            'password': os.getenv('db_password')
+        }
+        self.cnx_pool = mysql.connector.pooling.MySQLConnectionPool(
+            pool_name = 'conn_pool',
+            pool_size = 15,
+            **db_config
+        )
 
-	def get_connection_and_cursor(self):
-		conn = self.cnx_pool.get_connection()
-		cursor = conn.cursor()
-		return conn, cursor
+    def get_connection_and_cursor(self):
+        conn = self.cnx_pool.get_connection()
+        cursor = conn.cursor()
+        return conn, cursor
 
-	def release_connection_and_cursor(self, conn, cursor):
-		cursor.close()
-		conn.close()
+    def release_connection_and_cursor(self, conn, cursor):
+        cursor.close()
+        conn.close()
